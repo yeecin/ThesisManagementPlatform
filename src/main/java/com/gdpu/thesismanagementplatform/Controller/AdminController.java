f3fac99a3104c644c3a7c37e3db63a42299eb3d0
yeecin <13727679962@163.com>
2024-05-14 21:44:16 +0800

    Changes

--- src/main/java/com/gdpu/thesismanagementplatform/Controller/AdminController.java
+++ src/main/java/com/gdpu/thesismanagementplatform/Controller/AdminController.java
@@ -5,6 +5,7 @@ import com.gdpu.thesismanagementplatform.pojo.Teacher;
 import com.gdpu.thesismanagementplatform.repository.StudentRepository;
 import com.gdpu.thesismanagementplatform.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,5 +29,15 @@ public class AdminController {
     @ResponseBody
     public List<Teacher> getTeachers() {
         return teacherRepository.findAll();
+    }
+    @RequestMapping("/searchStudentById")
+    @ResponseBody
+    public Student searchStudent(@Param("studentId") int studentId) {
+        return studentRepository.findByStudentId(studentId);
+    }
+    @RequestMapping("/searchTeacherById")
+    @ResponseBody
+    public Teacher searchTeacher(@Param("teacherId") int teacherId) {
+        return teacherRepository.findByTeacherId(teacherId);
     }
 }
--- src/main/java/com/gdpu/thesismanagementplatform/repository/StudentRepository.java
+++ src/main/java/com/gdpu/thesismanagementplatform/repository/StudentRepository.java
@@ -2,10 +2,14 @@ package com.gdpu.thesismanagementplatform.repository;
 
 import com.gdpu.thesismanagementplatform.pojo.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, String> {
     Student findByStudentNameAndPassword(String studentName, String password);
     Student findByStudentName(String studentName);
-
     Student findByStudentId(int studentId);
+
 }
