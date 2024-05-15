90386d3a1def446a5d0e9a78a1d4709b8a76ae0d
yeecin <13727679962@163.com>
2024-05-15 17:54:43 +0800

    Changes

---
+++ src/main/java/com/gdpu/thesismanagementplatform/repository/ThesisChoiceRepository.java
@@ -0,0 +1,8 @@
+package com.gdpu.thesismanagementplatform.repository;
+
+import com.gdpu.thesismanagementplatform.pojo.ThesisChoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ThesisChoiceRepository extends JpaRepository<ThesisChoice, Integer>{
+
+}
